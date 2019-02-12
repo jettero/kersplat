138c6b006cee68455fbaefcc692da16b783345d7
Paul Miller <paul@jettero.pl>
2019-02-12 11:48:04 -0500

    move some trivial things around

--- centos/docker-entrypoint.sh
+++ centos/docker-entrypoint.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-cat > /usr/bin/get-hubble << EOF
-git clone https://github.com/hubblestack/hubble -o github /hubble
-git clone https://github.com/hubblestack/hubblestack_data -o github /hubblestack_data
-EOF
-chmod 0755 /usr/bin/get-hubble
-
-cat > /root/.bashrc << EOF 
-export PATH="\$HOME/.pyenv/bin:\$PATH"
-eval "\$(pyenv init -)"
-PS1='hubble-docker \\w\\$ '
-alias ls='ls -h --color=tty'
-alias l='ls -sh'
-cd /hubble
-EOF
 source /root/.bashrc
-pyenv local $PY_V
-pyenv shell $PY_V
 
 echo "entrypoint finished. issuing CMD: $*"
 "$@"
