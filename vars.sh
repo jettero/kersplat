11ff376f1fce0e4977c351f9574422c3b0f05b02
Paul Miller <paul@jettero.pl>
2019-02-19 09:05:05 -0500

    update the vars.sh to support hubblestack/centos7:latest

--- vars.sh
+++ vars.sh
@@ -3,26 +3,34 @@
 TOP_FILE="$(realpath "$0")"
 TOP_DIR="$(dirname "$TOP_FILE")"
 
+source "$TOP_DIR/func.sh"
+
 DHUB_USER="${DHUB_USER:-$USER}"
 DHUB_NAME="$(basename "$TOP_DIR")"
 DHUB_REPO="$DHUB_USER/$DHUB_NAME"
 
 IV="$(git describe --always)"
-OS="${OS:-centos}"
-OS_DIR="$TOP_DIR/$OS"
-OSV="${OSV:-latest}"
+OSI="${OSI:-centos:latest}"
+OSO="${OSI%%/*}"
+OSRV="${OSI##*/}"
+OSV="${OSRV##*:}"
+OSR="${OSRV%%:*}"
+OSD="$OSO${OSO:+-}$OSR"; OS_DIR="$TOP_DIR/$OSD"
 
-if [[ "$OS" =~ : ]]; then
-    OSV="$( cut -d: -f2 <<< "$OS" )"
-     OS="$( cut -d: -f1 <<< "$OS" )"
-fi
-
-IMAGE_TAG="$OS-$IV"
+IMAGE_TAG="$OSD-$IV"
 IMAGE_NAME="$DHUB_REPO:$IMAGE_TAG"
-CONTAINER_NAME="$( sed -e 's/[^a-zA-Z0-9][^a-zA-Z0-9]*/-/g' <<< "$IMAGE_NAME" )"
+CONTAINER_NAME="$IMAGE_TAG"
+
+var_color=$'\e[0;35m'
+val_color=$'\e[1;35m'
+rst=$'\e[m'
 
 echo "${var_color}TOP_DIR:        ${val_color}${TOP_DIR}${rst}"
-echo "${var_color}OS:OSV:         ${val_color}${OS}:${OSV}${rst}"
+echo "${var_color}OSI:            ${val_color}${OSI}${rst}"
+echo "${var_color}OSRV:           ${val_color}${OSRV}${rst}"
+echo "${var_color}OSO:            ${val_color}${OSO}${rst}"
+echo "${var_color}OSR:            ${val_color}${OSR}${rst}"
+echo "${var_color}OSV:            ${val_color}${OSV}${rst}"
 echo "${var_color}OS_DIR:         ${val_color}${OS_DIR}${rst}"
 echo "${var_color}DHUB_USER:      ${val_color}${DHUB_USER}${rst}"
 echo "${var_color}DHUB_NAME:      ${val_color}${DHUB_NAME}${rst}"
@@ -30,5 +38,7 @@ echo "${var_color}DHUB_REPO:      ${val_color}${DHUB_REPO}${rst}"
 echo "${var_color}IMAGE_TAG:      ${val_color}${IMAGE_TAG}${rst}"
 echo "${var_color}IMAGE_NAME:     ${val_color}${IMAGE_NAME}${rst}"
 echo "${var_color}CONTAINER_NAME: ${val_color}${CONTAINER_NAME}${rst}"
+
+unset var_color val_color rst
 
 [ -n "$VAR_ONLY" ] && exit 0
