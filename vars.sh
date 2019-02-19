3e20ad30c644794b580f46106598a3e53ae227da
Paul Miller <paul@jettero.pl>
2019-02-19 09:16:16 -0500

    bugs

--- vars.sh
+++ vars.sh
@@ -11,11 +11,16 @@ DHUB_REPO="$DHUB_USER/$DHUB_NAME"
 
 IV="$(git describe --always)"
 OSI="${OSI:-centos:latest}"
-OSO="${OSI%%/*}"
-OSRV="${OSI##*/}"
-OSV="${OSRV##*:}"
-OSR="${OSRV%%:*}"
-OSD="$OSO${OSO:+-}$OSR"; OS_DIR="$TOP_DIR/$OSD"
+if [[ "$OSI" =~ / ]]
+then OSO="${OSI%%/*}"
+else OSO=""
+fi
+tmp="${OSI##*/}"
+OSR="${tmp%%:*}"
+OSV="${tmp##*:}"
+echo "WTF!! OSD=OSO=$OSO[[${OSO:+-}]]OSR=$OSR"
+OSD="$OSO${OSO:+-}$OSR"
+OS_DIR="$TOP_DIR/$OSD"
 
 IMAGE_TAG="$OSD-$IV"
 IMAGE_NAME="$DHUB_REPO:$IMAGE_TAG"
@@ -27,11 +32,10 @@ rst=$'\e[m'
 
 echo "${var_color}TOP_DIR:        ${val_color}${TOP_DIR}${rst}"
 echo "${var_color}OSI:            ${val_color}${OSI}${rst}"
-echo "${var_color}OSRV:           ${val_color}${OSRV}${rst}"
 echo "${var_color}OSO:            ${val_color}${OSO}${rst}"
 echo "${var_color}OSR:            ${val_color}${OSR}${rst}"
 echo "${var_color}OSV:            ${val_color}${OSV}${rst}"
-echo "${var_color}OS_DIR:         ${val_color}${OS_DIR}${rst}"
+echo "${var_color}OSD:            ${val_color}${OSD}${rst}"
 echo "${var_color}DHUB_USER:      ${val_color}${DHUB_USER}${rst}"
 echo "${var_color}DHUB_NAME:      ${val_color}${DHUB_NAME}${rst}"
 echo "${var_color}DHUB_REPO:      ${val_color}${DHUB_REPO}${rst}"
@@ -39,6 +43,6 @@ echo "${var_color}IMAGE_TAG:      ${val_color}${IMAGE_TAG}${rst}"
 echo "${var_color}IMAGE_NAME:     ${val_color}${IMAGE_NAME}${rst}"
 echo "${var_color}CONTAINER_NAME: ${val_color}${CONTAINER_NAME}${rst}"
 
-unset var_color val_color rst
+unset var_color val_color rst tmp
 
 [ -n "$VAR_ONLY" ] && exit 0
