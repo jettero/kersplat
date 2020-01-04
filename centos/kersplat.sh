97b9c43f36e7f3ac280e8cdbf86099cc9cdbe2ae
Paul Miller <paul@jettero.pl>
2020-01-04 16:13:23 -0500

    a minor thing really

--- centos/kersplat.sh
+++ centos/kersplat.sh
@@ -2,8 +2,9 @@
 
 echo "/etc/profile.d/kersplat.sh"
 
+export PY_V=$(< /etc/PY_V)
 export PATH="$PYENV_ROOT/bin:$PATH"
-export PS1="\u[$PY_V] \w\$ "
+export PS1="\u[\$(pyenv version-name)] \w\$ "
 
 alias ls='ls -h --color=tty'
 alias l='ls -sh'
