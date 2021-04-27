c40c0061dc142409ea0869a24f37f0126d39bb4e
Paul Miller <paul@jettero.pl>
2021-04-27 09:06:03 -0400

    note to self

--- upload.sh
+++ upload.sh
@@ -6,3 +6,7 @@ dprune
 dbuild
 set -x
 docker image push $IMAGE_NAME
+
+# NOTE TO SELF:
+# docker image tag jettero/kersplat:centos-v1.0.18 hubblestack/jenkins:centos-v1.0.18
+# docker image push hubblestack/jenkins:centos-v1.0.18
